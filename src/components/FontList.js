808f7706dad3601e62d1bf48ed1654e097ac456b
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-03 22:23:44 +0200

    almost

--- src/components/FontList.js
+++ src/components/FontList.js
@@ -1,10 +1,9 @@
 import Font from './Font'
 const FontList = ({ fonts, text, size, filter }) => {
-  console.log(filter[0])
   return (
     <div className="col-lg-9">
       <h2 className="mb-3">
-        <span className="badge bg-danger">{filter[0]}</span>
+        <span className="badge bg-danger">{filter.txt}</span>
       </h2>
       <section className="row mb-5">
         {fonts.map(el => { return < Font key={el.family} elFont={el} text={text} size={size} /> }
--- src/components/FontsApp.js
+++ src/components/FontsApp.js
@@ -6,11 +6,11 @@ const FontsApp = () => {
   const [fonts, setFonts] = useState([])
   const [error, setError] = useState("")
   const [text, setText] = useState("Portez ce vieux whisky au juge blond qui fume !? 0123456789")
-  const [filter, setFilter] = useState(["Les plus récentes", 'date'])
+  const [filter, setFilter] = useState({ txt: "Les plus récentes", id: 'date' })
   const [size, setSize] = useState(20)
 
   useEffect(() => {
-    const url = `https://www.googleapis.com/webfonts/v1/webfonts?key=${process.env.REACT_APP_GOOGLE_API_KEY}&sort=${filter[1]}`
+    const url = `https://www.googleapis.com/webfonts/v1/webfonts?key=${process.env.REACT_APP_GOOGLE_API_KEY}&sort=${filter.id}`
     fetch(url)
       .then(response => {
         if (!response.ok) {
@@ -19,7 +19,7 @@ const FontsApp = () => {
         return response.json()
       })
       .then(data => {
-        setFonts(data.items.slice(0,10))
+        setFonts(data.items.slice(0, 10))
       })
       .catch((e) => {
         setError(e.message)
