a2df9d25387bdfba63324ec8af3a147475a6c2d4
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-03 18:48:03 +0200

    okkk

--- src/components/Font.js
+++ src/components/Font.js
@@ -1,13 +1,14 @@
-const Font = ({ font }) => {
+const Font = ({ font, text }) => {
+  console.log(text)
   return (
     <article className="col-lg-6 mb-4">
       <div className="shadow-sm border p-3 h-100">
         <h2 className="h6 d-flex aling-items-center justify-content-between">
           <span>{font.family}</span><small>{font.variants.length} variant(s)</small></h2>
-          <p><span className="badge bg-dark">{font.category}</span></p>
-          <p style={{'fontFamily': 'Karantina', 'fontSize': '20px'}} className="sample">Portez ce vieux whisky au juge blond qui fume !? 0123456789</p>
-          <a rel="noopener noreferrer" target="_blank" className="text-danger" href={`https://fonts.google.com/specimen/${font.family}`}>Voir sur Google Fonts (ouvre un nouveau tab)</a>
-        </div>
+        <p><span className="badge bg-dark">{font.category}</span></p>
+        <p style={{ 'fontFamily': 'Karantina', 'fontSize': '20px' }} className="sample">{text}</p>
+        <a rel="noopener noreferrer" target="_blank" className="text-danger" href={`https://fonts.google.com/specimen/${font.family}`}>Voir sur Google Fonts (ouvre un nouveau tab)</a>
+      </div>
     </article>
   );
 };
--- src/components/FontsApp.js
+++ src/components/FontsApp.js
@@ -5,6 +5,10 @@ import { useEffect, useState } from 'react';
 const FontsApp = () => {
   const [fonts, setFonts] = useState([])
   const [error, setError] = useState("")
+  const [text, setText] = useState("Portez ce vieux whisky au juge blond qui fume !? 0123456789")
+  const [filter, setFilter] = useState("default")
+  const [size, setSize] = useState(20)
+
   useEffect(() => {
     const url = `https://www.googleapis.com/webfonts/v1/webfonts?key=${process.env.REACT_APP_GOOGLE_API_KEY}`
     fetch(url)
@@ -20,7 +24,6 @@ const FontsApp = () => {
       .catch((e) => {
         setError(e.message)
       }
-
       )
   }, [])
 
@@ -30,8 +33,15 @@ const FontsApp = () => {
       {!!error &&
         <div className="alert alert-danger mt-3 text-center" >{error}</div>
       }
-      <TrendingSelect fonts={fonts} setFonts={setFonts} />
-      <FontList fonts={fonts} />
+      <TrendingSelect
+        filter={filter}
+        setFilter={setFilter}
+        text={text}
+        setText={setText}
+        size={size}
+        setSize={setSize}
+      />
+      <FontList fonts={fonts} text={text} />
     </div>
   );
 };
