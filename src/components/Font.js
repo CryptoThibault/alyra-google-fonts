bc3d12810da46fb118dad5fbff20cadc8c302b9f
Benmissi-A <aminebenmissi@hotmail.fr>
2021-05-03 19:31:42 +0200

    push

--- src/components/Font.js
+++ src/components/Font.js
@@ -6,8 +6,8 @@ const Font = ({ font, text }) => {
         <h2 className="h6 d-flex aling-items-center justify-content-between">
           <span>{font.family}</span><small>{font.variants.length} variant(s)</small></h2>
         <p><span className="badge bg-dark">{font.category}</span></p>
-        <p style={{ 'fontFamily': 'Karantina', 'fontSize': '20px' }} className="sample">{text}</p>
-        <a rel="noopener noreferrer" target="_blank" className="text-danger" href={`https://fonts.google.com/specimen/${font.family}`}>Voir sur Google Fonts (ouvre un nouveau tab)</a>
+        <p style={{ 'fontFamily': 'Oi', 'fontSize': '20px' }} className="sample">{text}</p>
+        <a rel="noopener noreferrer" ahianatarget="_blank" className="text-danger" href={`https://fonts.google.com/specimen/${font.family}`}>Voir sur Google Fonts (ouvre un nouveau tab)</a>
       </div>
     </article>
   );
