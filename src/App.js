62e9a9f004b7a24a7bf3f0a319591b67f5f83e58
CryptoThibault <thibault.chalaoux@gmail.com>
2021-05-04 15:29:59 +0200

    darkmode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import React, { useState } from 'react'
 import Header from './components/Header'
 import FontApp from './components/FontsApp'
 import Footer from './components/Footer'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
   return (
     <div className="App">
-      <Header />
-      <FontApp />
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <FontApp darkMode={darkMode} />
       <Footer />
     </div>
   );
